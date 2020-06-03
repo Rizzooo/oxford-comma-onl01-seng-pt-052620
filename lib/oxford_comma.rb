9a83ab3b0d102d2c9feadc0be8bf6190f6e1df1a
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-03 22:10:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ def oxford_comma(array)
   if array.length == 1
   array.first 
   elsif array.length == 2
-  array.join(", ")
+  array.join(" and ")
   elsif array.length >= 3
   array[array.length - 1] = "and #{array[array.length - 1]}"
   array.join(", ")
