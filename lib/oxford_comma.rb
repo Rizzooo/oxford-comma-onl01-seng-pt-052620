05e7e7c0677288472468e9cae74c2cf1d97a388b
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-03 22:15:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+#def oxford_comma(array)
+ # array.join
+#end
 
+#def oxford_comma(array)
+ # array.join(" and ")
+#end
+
+def oxford_comma(array)
+  if array.length == 1
+  array.first 
+  elsif array.length == 2
+  array.join(" and ")
+  elsif array.length >= 3
+  array[array.length - 1] = "and #{array[array.length - 1]}"
+  array.join(", ")
+  end
 end
+  
+  
